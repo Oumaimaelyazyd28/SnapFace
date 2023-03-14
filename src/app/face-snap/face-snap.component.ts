73321e76b07a6c1ed38141537c8671b676937fbd
Oumaima Elyazyd <elyazydoumaima2000@gmail.com>
2023-03-14 11:18:24 +0100

    add and configure faceSnap service

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit{
+  @Input() faceSnap!: FaceSnap;
 
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string
+  buttonText!: string
+
+  ngOnInit(){
+    this.title = "Sii";
+    this.description = 'Mon meuilleur ami depuis tout petit!';
+    this.createdDate = new Date();
+    this.snaps = 8;
+    this.imageUrl = 'D:\INGECYC\cat-g53df6867e_1920.jpg'
+    this.buttonText = 'Oh Snap!'
+  }
+
+  onSnap(){
+    if(this.buttonText === 'Oh Snap!') {
+      this.faceSnap.snaps++;
+      this.buttonText = 'Oops, unSnap!';
+    } else {
+      this.faceSnap.snaps--;
+      this.buttonText = 'Oh Snap!';
+    }
+  }
 }
---
+++ src/app/models/face-snap.model.ts
@@ -0,0 +1,67 @@
+export class FaceSnap{
+    title!: string;
+    description!: string;
+    imageUrl!: string;
+    createdDate!: Date;
+    snaps!: number;
+    location?: string;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Vous pouvez utiliser cette methode si vous avez des propriétés qui seront initialisées par 
+//les arguments passés au constructor comme dans la partie en commentaire
+/*export class FaceSnap{
+
+    constructor(public title: string, 
+                public description: string, 
+                public imageUrl: string, 
+                public createdDate: Date, 
+                public snaps: number,
+                public location?: string){}
+}
+
+export class FaceSnap{
+    title: string;
+    description: string;
+    imageUrl: string;
+    createdDate: Date;
+    snaps: number;
+
+    constructor(title: string, description: string, imageUrl: string, createdDate: Date, snaps: number){
+        this.title = title;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.createdDate = createdDate;
+        this.snaps = snaps;
+
+    }
+}*/
